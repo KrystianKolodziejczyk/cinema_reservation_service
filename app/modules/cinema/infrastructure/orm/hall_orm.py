bdc64822782e10e0cfc23e495da9635e4e444a79
Krystian Kolodziejczyk <krystiankolodziejczyk2004@gmail.com>
2026-05-26 10:17:16 +0200

    feat: add proper constrains to hall and movie tables

--- app/modules/cinema/infrastructure/orm/hall_orm.py
+++ app/modules/cinema/infrastructure/orm/hall_orm.py
@@ -1,5 +1,5 @@
 from shared.db_client.base_orm import Base
-from sqlalchemy import CheckConstraint, Integer, String
+from sqlalchemy import CheckConstraint, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -7,13 +7,16 @@ class HallORM(Base):
     __tablename__ = "halls"
 
     hall_id: Mapped[int] = mapped_column(primary_key=True)
-    hall_name: Mapped[str] = mapped_column(String(10), unique=True)
-    rows: Mapped[int] = mapped_column(Integer)
-    seats_per_row: Mapped[int] = mapped_column(Integer)
+    hall_name: Mapped[str] = mapped_column(String(15), unique=True)
+    rows: Mapped[int]
+    seats_per_row: Mapped[int]
 
     __table_args__ = (
-        CheckConstraint("rows > 0 AND rows <= 15", "ck_rows_count"),
+        CheckConstraint("rows > 0 AND rows <= 15", name="ck_rows_count"),
         CheckConstraint(
-            "LENGTH(hall_name) > 0 AND LENGTH(hall_name) <= 15", "ck_hall_name"
+            "LENGTH(hall_name) > 0 AND LENGTH(hall_name) <= 15", name="ck_hall_name"
+        ),
+        CheckConstraint(
+            "seats_per_row > 0 AND seats_per_row <= 20", name="ck_hall_seats_per_row"
         ),
     )
--- app/modules/cinema/infrastructure/orm/movie_orm.py
+++ app/modules/cinema/infrastructure/orm/movie_orm.py
@@ -1,5 +1,5 @@
 from shared.db_client.base_orm import Base
-from sqlalchemy import Index
+from sqlalchemy import CheckConstraint, Index
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import String
 
@@ -9,16 +9,23 @@ class MovieORM(Base):
 
     movie_id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
-    description: Mapped[str]
+    description: Mapped[str | None] = mapped_column(default=None)
     director: Mapped[str] = mapped_column(String(100))
     duration: Mapped[int]
     genre: Mapped[str] = mapped_column(String(50))
-    rating: Mapped[float]
-    poster_url: Mapped[str]
+    rating: Mapped[float | None] = mapped_column(default=None)
+    poster_url: Mapped[str | None] = mapped_column(default=None)
 
     __table_args__ = (
         Index("idx_movie_title", "title"),
         Index("idx_movie_genre", "genre"),
         Index("idx_movie_director", "director"),
         Index("idx_movie_rating", "rating"),
+        CheckConstraint("LENGTH(title) > 2", name="ck_movie_title"),
+        CheckConstraint("duration > 0", name="ck_movie_duration"),
+        CheckConstraint("rating >= 0.0 AND rating <= 5.0", name="ck_movie_rating"),
+        CheckConstraint(
+            "LENGTH(description) > 2 AND LENGTH(description) <= 500",
+            name="ck_movie_descritpion",
+        ),
     )
